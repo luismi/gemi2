fd6e83654b0733afee462cbd60b521c28975ddbc
luismi <luismi@MacBook-Air-de-Luismi.local>
2024-05-15 09:16:40 +0200

    Add download button

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # Import the required libraries
+import os
+import json
+import pandas as pd
 import google.generativeai as genai
 import streamlit as st
 from scrapegraphai.graphs import SmartScraperGraph
-from scrapegraphai.utils import prettify_exec_info
+
 
 # Get OpenAI API key from user
 openai_access_token = st.text_input("OpenAI API Key", type="password")
 
+
 if openai_access_token:
-    model = "gemini-pro"
+    model = "gemini-1.5-flash-latest"
     
     graph_config = {
         "llm": {
@@ -18,6 +22,7 @@ if openai_access_token:
     }
     # Get the URL of the website to scrape
     url = st.text_input("Enter the URL of the website you want to scrape")
+    
     # Get the user prompt
     user_prompt = st.text_input("What you want the AI agent to scrae from the website?")
     
@@ -27,11 +32,32 @@ if openai_access_token:
         source=url,
         config=graph_config
     )
+
+    # Added downloads buttons
+    def add_download_options(result: str):
+        """
+        Adds download buttons for graph result.
+        """
+        st.download_button(
+            label="Download JSON",
+            data=json.dumps(result, indent=4),
+            file_name="scraped_data.json",
+            mime="application/json"
+        )
+
+        df = pd.DataFrame(result)
+        csv = df.to_csv(index=False)
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            file_name="scraped_data.csv",
+            mime="text/csv"
+        )
+        
     # Scrape the website
     if st.button("Scrape"):
         result = smart_scraper_graph.run()
-        st.write(result)
+        st.write("# Respuesta")
+        st.write(graph_result)
+        add_download_options(graph_result)
         
-        # Get graph execution info
-        graph_exec_info = smart_scraper_graph.get_execution_info()
-        st.write(prettify_exec_info(graph_exec_info))
