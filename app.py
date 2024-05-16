6fa52948c78664a5c0025aae8200b7fba74eee62
luismi <luismi@MacBook-Air-de-Luismi.local>
2024-05-16 11:24:56 +0200

    Mochos Cambios

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Import the required libraries
 import os
 import json
-import pandas as pd
 import google.generativeai as genai
 import streamlit as st
 from scrapegraphai.graphs import SmartScraperGraph
@@ -10,33 +9,13 @@ from scrapegraphai.graphs import SmartScraperGraph
 # Get OpenAI API key from user
 openai_access_token = st.text_input("OpenAI API Key", type="password")
 
-# Added downloads buttons
-def add_download_options(result: str):
-    """
-    Adds download buttons for graph result.
-    """
-    st.download_button(
-        label="Download JSON",
-        data=json.dumps(result, indent=4),
-        file_name="scraped_data.json",
-        mime="application/json"
-    )
-
-    df = pd.DataFrame(result)
-    csv = df.to_csv(index=False)
-    st.download_button(
-        label="Download CSV",
-        data=csv,
-        file_name="scraped_data.csv",
-        mime="text/csv"
-    )
 if openai_access_token:
     model = "gemini-pro"
     
     graph_config = {
         "llm": {
             "api_key": openai_access_token,
-            "model": model
+            "model": model,
         },
     }
     # Get the URL of the website to scrape
@@ -57,5 +36,9 @@ if openai_access_token:
         result = smart_scraper_graph.run()
         st.write("# Respuesta")
         st.write(result)
-        add_download_options(result)
-        
+    
+    if st.download_button("# Download JSON"):
+        data=json.dumps(result, indent=4),
+        file_name="scraped_data.json",
+        mime="application/json"
+
